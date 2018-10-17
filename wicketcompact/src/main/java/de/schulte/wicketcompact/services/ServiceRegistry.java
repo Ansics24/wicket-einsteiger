fe76616749ad0e7f1355e7b6fe6cf4ccfe7d4675
Ansgar Schulte <info@ansgarschulte-itconsulting.de>
2018-10-17 22:52:02 +0200

    service registry added for getting service instances

--- wicketcompact/src/main/java/de/schulte/wicketcompact/services/ServiceRegistry.java
+++ wicketcompact/src/main/java/de/schulte/wicketcompact/services/ServiceRegistry.java
@@ -1,4 +1,26 @@
 package de.schulte.wicketcompact.services;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ServiceRegistry {
+
+    private static final Map<Class<? extends BaseService>, BaseService> SERVICES = new HashMap<>();
+
+    private ServiceRegistry() {
+    }
+
+    public static <T extends BaseService> T get(Class<T> serviceClass) {
+        BaseService service = SERVICES.get(serviceClass);
+        if (service == null) {
+            try {
+                service = serviceClass.newInstance();
+                SERVICES.put(serviceClass, service);
+            } catch (InstantiationException | IllegalAccessException e) {
+                throw new RuntimeException("Service konnte nicht erstellt werden", e);
+            }
+        }
+        return (T) service;
+    }
+
 }
