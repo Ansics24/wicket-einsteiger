85e1664b072dc205ff93b7bf1909830d3363cf92
Ansgar Schulte <info@ansgarschulte-itconsulting.de>
2018-10-26 10:46:15 +0200

    added logo with ContextRelativeResourceReference and Image

--- wicketcompact/src/main/java/de/schulte/wicketcompact/Header.java
+++ wicketcompact/src/main/java/de/schulte/wicketcompact/Header.java
@@ -1,5 +1,7 @@
 package de.schulte.wicketcompact;
 
+import de.schulte.wicketcompact.resources.LogoResourceReference;
+import org.apache.wicket.markup.html.image.Image;
 import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.markup.html.panel.Panel;
@@ -12,5 +14,6 @@ public class Header extends Panel {
         add(new BookmarkablePageLink<Void>("categories", CategoriesPage.class));
         add(new BookmarkablePageLink<Void>("articles", ArticlesPage.class));
         add(new BookmarkablePageLink<Void>("tables", TablesPage.class));
+        add(new Image("brand", new LogoResourceReference()));
     }
 }
---
+++ wicketcompact/src/main/java/de/schulte/wicketcompact/resources/LogoResourceReference.java
@@ -0,0 +1,10 @@
+package de.schulte.wicketcompact.resources;
+
+import org.apache.wicket.request.resource.ContextRelativeResourceReference;
+
+public class LogoResourceReference extends ContextRelativeResourceReference {
+
+    public LogoResourceReference() {
+        super("sg-logo.png");
+    }
+}
