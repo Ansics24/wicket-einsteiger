e85a79d3c8f7ab310f2657a11c0fc54462a39811
Ansgar Schulte <info@ansgarschulte-itconsulting.de>
2018-10-26 12:41:28 +0200

    introduced simple bootstrap template and first markup inheritance

--- wicketcompact/src/main/java/de/schulte/wicketcompact/Header.java
+++ wicketcompact/src/main/java/de/schulte/wicketcompact/Header.java
@@ -1,17 +1,9 @@
 package de.schulte.wicketcompact;
 
-import de.schulte.wicketcompact.entities.Article;
-import org.apache.wicket.MarkupContainer;
 import org.apache.wicket.markup.html.image.ContextImage;
-import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 import org.apache.wicket.markup.html.panel.Panel;
 
 public class Header extends Panel {
-
-    private  MarkupContainer dashboardLink;
-    private  MarkupContainer categoriesLink;
-    private  MarkupContainer articlesLink;
-    private  MarkupContainer tablesLink;
 
     public Header(String id) {
         super(id);
@@ -20,10 +12,10 @@ public class Header extends Panel {
     @Override
     protected void onInitialize() {
         super.onInitialize();
-        dashboardLink = add(new BookmarkablePageLink<Void>("dashboard", getApplication().getHomePage()).setEnabled(!getPage().getClass().equals(HomePage.class)));
-        categoriesLink = add(new BookmarkablePageLink<Void>("categories", CategoriesPage.class).setEnabled(!getPage().getClass().equals(CategoriesPage.class)));
-        articlesLink = add(new BookmarkablePageLink<Void>("articles", ArticlesPage.class).setEnabled(!getPage().getClass().equals(ArticlesPage.class)));
-        tablesLink = add(new BookmarkablePageLink<Void>("tables", TablesPage.class).setEnabled(!getPage().getClass().equals(TablesPage.class)));
+        add(new PageLink("dashboard", getApplication().getHomePage()));
+        add(new PageLink("categories", CategoriesPage.class));
+        add(new PageLink("articles", ArticlesPage.class));
+        add(new PageLink("tables", TablesPage.class));
         add(new ContextImage("brand", "sg-logo.png"));
     }
 }
---
+++ wicketcompact/src/main/java/de/schulte/wicketcompact/PageLink.java
@@ -0,0 +1,20 @@
+package de.schulte.wicketcompact;
+
+import org.apache.wicket.Page;
+import org.apache.wicket.markup.html.link.BookmarkablePageLink;
+
+public class PageLink extends BookmarkablePageLink<Void> {
+
+    private final Class<? extends Page> pageClass;
+
+    public PageLink(String id, Class<? extends Page> pageClass) {
+        super(id, pageClass);
+        this.pageClass = pageClass;
+    }
+
+    @Override
+    protected void onInitialize() {
+        super.onInitialize();
+        setEnabled(!getPage().getClass().equals(this.pageClass));
+    }
+}
