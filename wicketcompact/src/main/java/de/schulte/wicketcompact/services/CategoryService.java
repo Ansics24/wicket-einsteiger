9c7f108b5d72d54eddcbc99bfc5b583cb136b309
Ansgar Schulte <info@ansgarschulte-itconsulting.de>
2018-10-28 20:58:21 +0100

    added initial categories to category service

--- wicketcompact/src/main/java/de/schulte/wicketcompact/services/CategoryService.java
+++ wicketcompact/src/main/java/de/schulte/wicketcompact/services/CategoryService.java
@@ -3,4 +3,16 @@ package de.schulte.wicketcompact.services;
 import de.schulte.wicketcompact.entities.Category;
 
 public class CategoryService extends BaseService<Category> {
+
+    public CategoryService() {
+        final Category coffee = new Category();
+        coffee.setName("Kaffee");
+        final Category drinks = new Category();
+        drinks.setName("Alkoholfreie Getränke");
+        final Category beer = new Category();
+        beer.setName("Biere");
+        save(coffee);
+        save(drinks);
+        save(beer);
+    }
 }
