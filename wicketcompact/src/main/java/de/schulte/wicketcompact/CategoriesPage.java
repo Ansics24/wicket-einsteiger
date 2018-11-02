81db73bea7eea1713fcaa73955f245cfc18d78bb
Ansgar Schulte <info@ansgarschulte-itconsulting.de>
2018-11-02 21:38:53 +0100

    added image urls for predefined categories / categories as ListView

--- wicketcompact/src/main/java/de/schulte/wicketcompact/CategoriesPage.java
+++ wicketcompact/src/main/java/de/schulte/wicketcompact/CategoriesPage.java
@@ -3,9 +3,15 @@ package de.schulte.wicketcompact;
 import de.schulte.wicketcompact.entities.Category;
 import de.schulte.wicketcompact.services.CategoryService;
 import de.schulte.wicketcompact.services.ServiceRegistry;
+import org.apache.wicket.behavior.AttributeAppender;
+import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
-import org.apache.wicket.markup.repeater.RepeatingView;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CategoriesPage extends BaseEntitiesPage {
 
@@ -16,10 +22,17 @@ public class CategoriesPage extends BaseEntitiesPage {
     @Override
     protected void onInitialize() {
         super.onInitialize();
-        final RepeatingView categories = new RepeatingView("categories");
-        for (Category category : ServiceRegistry.get(CategoryService.class).listAll()) {
-            categories.add(new Label(categories.newChildId(), category.getName()));
-        }
+        final List<Category> categoryList = new ArrayList<>(ServiceRegistry.get(CategoryService.class).listAll());
+        final ListView<Category> categories = new ListView<Category>("categories", categoryList) {
+
+            @Override
+            protected void populateItem(ListItem<Category> item) {
+                item.add(new Label("name", item.getModelObject().getName()));
+
+                final AttributeAppender srcAppender = new AttributeAppender("src", item.getModelObject().getImageUrl());
+                item.add(new WebMarkupContainer("image").add(srcAppender));
+            }
+        };
         add(categories);
     }
 }
--- wicketcompact/src/main/java/de/schulte/wicketcompact/services/CategoryService.java
+++ wicketcompact/src/main/java/de/schulte/wicketcompact/services/CategoryService.java
@@ -7,10 +7,13 @@ public class CategoryService extends BaseService<Category> {
     public CategoryService() {
         final Category coffee = new Category();
         coffee.setName("Kaffee");
+        coffee.setImageUrl("https://images.freeimages.com/images/large-previews/5a3/raw-espresso-2-1177689.jpg");
         final Category drinks = new Category();
         drinks.setName("Alkoholfreie Getränke");
+        drinks.setImageUrl("https://images.freeimages.com/images/large-previews/9c2/coca-cola-1189757.jpg");
         final Category beer = new Category();
         beer.setName("Biere");
+        beer.setImageUrl("https://images.freeimages.com/images/large-previews/dff/three-champion-1328253.jpg");
         save(coffee);
         save(drinks);
         save(beer);
