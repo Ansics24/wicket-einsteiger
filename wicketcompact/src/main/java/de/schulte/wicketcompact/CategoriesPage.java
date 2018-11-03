2de4919c2fb062d6148648706a94c2da0f006c71
Ansgar Schulte <info@ansgarschulte-itconsulting.de>
2018-11-03 21:16:17 +0100

    categories implemented with DataView and PagingNavigator

--- wicketcompact/src/main/java/de/schulte/wicketcompact/CategoriesPage.java
+++ wicketcompact/src/main/java/de/schulte/wicketcompact/CategoriesPage.java
@@ -6,8 +6,11 @@ import de.schulte.wicketcompact.services.ServiceRegistry;
 import org.apache.wicket.behavior.AttributeAppender;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
-import org.apache.wicket.markup.html.list.ListItem;
-import org.apache.wicket.markup.html.list.ListView;
+import org.apache.wicket.markup.html.navigation.paging.PagingNavigator;
+import org.apache.wicket.markup.repeater.Item;
+import org.apache.wicket.markup.repeater.data.DataView;
+import org.apache.wicket.markup.repeater.data.IDataProvider;
+import org.apache.wicket.markup.repeater.data.ListDataProvider;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 
 import java.util.ArrayList;
@@ -23,16 +26,20 @@ public class CategoriesPage extends BaseEntitiesPage {
     protected void onInitialize() {
         super.onInitialize();
         final List<Category> categoryList = new ArrayList<>(ServiceRegistry.get(CategoryService.class).listAll());
-        final ListView<Category> categories = new ListView<Category>("categories", categoryList) {
+        final IDataProvider<Category> dataProvider = new ListDataProvider<>(categoryList);
+        final DataView<Category> categories = new DataView<Category>("categories", dataProvider) {
 
             @Override
-            protected void populateItem(ListItem<Category> item) {
+            protected void populateItem(Item<Category> item) {
                 item.add(new Label("name", item.getModelObject().getName()));
 
                 final AttributeAppender srcAppender = new AttributeAppender("src", item.getModelObject().getImageUrl());
                 item.add(new WebMarkupContainer("image").add(srcAppender));
             }
         };
+        categories.setItemsPerPage(3);
+        final PagingNavigator navigator = new PagingNavigator("navigator", categories);
         add(categories);
+        add(navigator);
     }
 }
--- wicketcompact/src/main/java/de/schulte/wicketcompact/services/CategoryService.java
+++ wicketcompact/src/main/java/de/schulte/wicketcompact/services/CategoryService.java
@@ -8,14 +8,37 @@ public class CategoryService extends BaseService<Category> {
         final Category coffee = new Category();
         coffee.setName("Kaffee");
         coffee.setImageUrl("https://images.freeimages.com/images/large-previews/5a3/raw-espresso-2-1177689.jpg");
+
+        final Category tea = new Category();
+        tea.setName("Tee");
+        tea.setImageUrl("https://images.freeimages.com/images/large-previews/ff1/tea-1327515.jpg");
+
         final Category drinks = new Category();
         drinks.setName("Alkoholfreie Getränke");
         drinks.setImageUrl("https://images.freeimages.com/images/large-previews/9c2/coca-cola-1189757.jpg");
+
         final Category beer = new Category();
         beer.setName("Biere");
         beer.setImageUrl("https://images.freeimages.com/images/large-previews/dff/three-champion-1328253.jpg");
+
+        final Category breakfast = new Category();
+        breakfast.setName("Frühstück");
+        breakfast.setImageUrl("https://images.freeimages.com/images/large-previews/def/sandwich-4-1525938.jpg");
+
+        final Category snacks = new Category();
+        snacks.setName("Snacks");
+        snacks.setImageUrl("https://images.freeimages.com/images/large-previews/0a6/butter-biscuits-1329768.jpg");
+
+        final Category burger = new Category();
+        burger.setName("Burger");
+        burger.setImageUrl("https://images.freeimages.com/images/large-previews/dbe/burger-on-a-plate-1328009.jpg");
+
         save(coffee);
+        save(tea);
         save(drinks);
         save(beer);
+        save(breakfast);
+        save(snacks);
+        save(burger);
     }
 }
