2b8bef696451c42e20049c313e090413113287a7
Ansgar Schulte <info@ansgarschulte-itconsulting.de>
2018-11-05 21:52:52 +0100

    added simple sorting by name to categorylist

---
+++ wicketcompact/src/main/java/de/schulte/wicketcompact/CategoriesDataProvider.java
@@ -0,0 +1,47 @@
+package de.schulte.wicketcompact;
+
+import de.schulte.wicketcompact.entities.Category;
+import de.schulte.wicketcompact.services.CategoryService;
+import de.schulte.wicketcompact.services.ServiceRegistry;
+import org.apache.wicket.extensions.markup.html.repeater.data.sort.SortOrder;
+import org.apache.wicket.extensions.markup.html.repeater.util.SortParam;
+import org.apache.wicket.extensions.markup.html.repeater.util.SortableDataProvider;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class CategoriesDataProvider extends SortableDataProvider<Category, String> {
+
+    public CategoriesDataProvider() {
+        setSort("name", SortOrder.ASCENDING);
+    }
+
+    @Override
+    public Iterator<? extends Category> iterator(long first, long count) {
+        final List<Category> allCategories = new ArrayList<>(ServiceRegistry.get(CategoryService.class).listAll());
+
+        final SortParam<String> sortOrder = getSort();
+        allCategories.sort((c1, c2) -> {
+            final String name1 = c1.getName();
+            final String name2 = c2.getName();
+            return sortOrder.isAscending() ? name1.compareTo(name2) : name2.compareTo(name1);
+        });
+
+        final List<Category> subList = allCategories.subList((int) first, (int) (first + count >= allCategories.size() ? allCategories.size() : first + count));
+        return subList.iterator();
+    }
+
+    @Override
+    public long size() {
+        return ServiceRegistry.get(CategoryService.class).listAll().size();
+    }
+
+    @Override
+    public IModel<Category> model(Category category) {
+        return Model.of(category);
+    }
+
+}
--- wicketcompact/src/main/java/de/schulte/wicketcompact/CategoriesPage.java
+++ wicketcompact/src/main/java/de/schulte/wicketcompact/CategoriesPage.java
@@ -1,36 +1,29 @@
 package de.schulte.wicketcompact;
 
 import de.schulte.wicketcompact.entities.Category;
-import de.schulte.wicketcompact.services.CategoryService;
-import de.schulte.wicketcompact.services.ServiceRegistry;
 import org.apache.wicket.behavior.AttributeAppender;
+import org.apache.wicket.extensions.markup.html.repeater.data.sort.OrderByBorder;
+import org.apache.wicket.extensions.markup.html.repeater.util.SortableDataProvider;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.navigation.paging.IPageable;
-import org.apache.wicket.markup.html.navigation.paging.PagingNavigation;
 import org.apache.wicket.markup.repeater.Item;
 import org.apache.wicket.markup.repeater.data.DataView;
-import org.apache.wicket.markup.repeater.data.IDataProvider;
-import org.apache.wicket.markup.repeater.data.ListDataProvider;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class CategoriesPage extends BaseEntitiesPage {
 
     private DataView<Category> categories;
+    private final SortableDataProvider<Category, String> dataProvider;
 
     public CategoriesPage(PageParameters parameters) {
         super(parameters);
-        final List<Category> categoryList = new ArrayList<>(ServiceRegistry.get(CategoryService.class).listAll());
-        final IDataProvider<Category> dataProvider = new ListDataProvider<>(categoryList);
+        dataProvider = new CategoriesDataProvider();
         categories = new DataView<Category>("categories", dataProvider) {
 
             @Override
             protected void populateItem(Item<Category> item) {
                 item.add(new Label("name", item.getModelObject().getName()));
-
                 final AttributeAppender srcAppender = new AttributeAppender("src", item.getModelObject().getImageUrl());
                 item.add(new WebMarkupContainer("image").add(srcAppender));
             }
@@ -47,6 +40,7 @@ public class CategoriesPage extends BaseEntitiesPage {
         super.onInitialize();
         categories.setItemsPerPage(3);
         add(categories);
+        add(new OrderByBorder<>("orderByName", "name", this.dataProvider));
     }
 
 
