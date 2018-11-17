ab7a3ebbb84fd3be1299d9a7a20fcc12ad560650
Ansgar Schulte <info@ansgarschulte-itconsulting.de>
2018-11-17 20:41:38 +0100

    modified ArticlesPage to link to new page for creating new articles

--- wicketcompact/src/main/java/de/schulte/wicketcompact/articles/ArticlesPage.java
+++ wicketcompact/src/main/java/de/schulte/wicketcompact/articles/ArticlesPage.java
@@ -2,48 +2,23 @@ package de.schulte.wicketcompact.articles;
 
 import de.schulte.wicketcompact.BaseEntitiesPage;
 import de.schulte.wicketcompact.EntityModel;
-import de.schulte.wicketcompact.SuccessFeedbackPanel;
-import de.schulte.wicketcompact.ValidationErrorFeedbackPanel;
 import de.schulte.wicketcompact.entities.Article;
-import de.schulte.wicketcompact.entities.Category;
 import de.schulte.wicketcompact.services.ArticleService;
-import de.schulte.wicketcompact.services.ServiceRegistry;
-import org.apache.wicket.bean.validation.PropertyValidator;
 import org.apache.wicket.behavior.AttributeAppender;
-import org.apache.wicket.extensions.markup.html.form.datetime.LocalDateTextField;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
-import org.apache.wicket.markup.html.form.*;
 import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.markup.html.navigation.paging.IPageable;
 import org.apache.wicket.markup.repeater.Item;
 import org.apache.wicket.markup.repeater.data.DataView;
 import org.apache.wicket.markup.repeater.data.IDataProvider;
 import org.apache.wicket.model.CompoundPropertyModel;
-import org.apache.wicket.model.Model;
 import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
-import org.apache.wicket.validation.validator.RangeValidator;
-import org.apache.wicket.validation.validator.UrlValidator;
-
-import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.time.format.FormatStyle;
 
 public class ArticlesPage extends BaseEntitiesPage {
 
     private final DataView<Article> articles;
-
-    private final Form<Article> form = new Form<Article>("form") {
-
-        @Override
-        protected void onSubmit() {
-            super.onSubmit();
-            form.setVisible(false);
-            ServiceRegistry.get(ArticleService.class).save(form.getModelObject());
-            form.success("Artikel wurde gespeichert");
-        }
-    };
 
     public ArticlesPage(PageParameters parameters) {
         super(parameters);
@@ -78,22 +53,6 @@ public class ArticlesPage extends BaseEntitiesPage {
                 form.setModelObject(new Article());
             }
         });
-        initializeForm();
-    }
-
-    private void initializeForm() {
-        form.setVisible(false);
-        add(form);
-        add(new ValidationErrorFeedbackPanel("validationFeedback"));
-        add(new SuccessFeedbackPanel("successFeedback"));
-        form.setModel(new CompoundPropertyModel<>(new EntityModel<>(ArticleService.class)));
-        form.add(new TextField<String>("name").setRequired(true));
-        form.add(new DropDownChoice<>("category", new CategoryListModel(), new ChoiceRenderer<Category>("name", "id")).add(new PropertyValidator<>()));
-        form.add(new TextArea<String>("description").setRequired(true).setLabel(Model.of("Beschreibung")));
-        form.add(new TextField<>("price").setRequired(true).setLabel(Model.of("Preis")).add(new RangeValidator<>(BigDecimal.ZERO, new BigDecimal("20"))));
-        form.add(new TextField<>("validFrom").setLabel(Model.of("Gültig ab")).add(RangeValidator.maximum(LocalDate.now().plusMonths(3))));
-        form.add(new TextField<>("validTo").setLabel(Model.of("Gültig bis")).add(RangeValidator.minimum(LocalDate.now().plusDays(1))));
-        form.add(new TextField<String>("imageUrl").setRequired(true).setLabel(Model.of("Bild-URL")).add(new UrlValidator()));
     }
 
     @Override
---
+++ wicketcompact/src/main/java/de/schulte/wicketcompact/articles/CreateArticlePage.java
@@ -0,0 +1,55 @@
+package de.schulte.wicketcompact.articles;
+
+import de.schulte.wicketcompact.BaseWebPage;
+import de.schulte.wicketcompact.EntityModel;
+import de.schulte.wicketcompact.SuccessFeedbackPanel;
+import de.schulte.wicketcompact.ValidationErrorFeedbackPanel;
+import de.schulte.wicketcompact.entities.Article;
+import de.schulte.wicketcompact.services.ArticleService;
+import de.schulte.wicketcompact.services.ServiceRegistry;
+import org.apache.wicket.bean.validation.PropertyValidator;
+import org.apache.wicket.markup.html.form.*;
+import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.model.CompoundPropertyModel;
+import org.apache.wicket.model.Model;
+import org.apache.wicket.validation.validator.RangeValidator;
+import org.apache.wicket.validation.validator.UrlValidator;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class CreateArticlePage extends BaseWebPage {
+
+    private final Form<Article> form = new Form<Article>("form") {
+
+        @Override
+        protected void onSubmit() {
+            super.onSubmit();
+            form.setVisible(false);
+            ServiceRegistry.get(ArticleService.class).save(form.getModelObject());
+            form.success("Artikel wurde gespeichert");
+        }
+    };
+
+    @Override
+    protected void onInitialize() {
+        super.onInitialize();
+        initializeForm();
+    }
+
+    private void initializeForm() {
+        form.setVisible(false);
+        add(form);
+        add(new ValidationErrorFeedbackPanel("validationFeedback"));
+        add(new SuccessFeedbackPanel("successFeedback"));
+        form.setModel(new CompoundPropertyModel<>(new EntityModel<>(ArticleService.class)));
+        form.add(new TextField<String>("name").setRequired(true));
+        form.add(new DropDownChoice<>("category", new CategoryListModel(), new ChoiceRenderer<>("name", "id")).add(new PropertyValidator<>()));
+        form.add(new TextArea<String>("description").setRequired(true).setLabel(Model.of("Beschreibung")));
+        form.add(new TextField<>("price").setRequired(true).setLabel(Model.of("Preis")).add(new RangeValidator<>(BigDecimal.ZERO, new BigDecimal("20"))));
+        form.add(new TextField<>("validFrom").setLabel(Model.of("Gültig ab")).add(RangeValidator.maximum(LocalDate.now().plusMonths(3))));
+        form.add(new TextField<>("validTo").setLabel(Model.of("Gültig bis")).add(RangeValidator.minimum(LocalDate.now().plusDays(1))));
+        form.add(new TextField<String>("imageUrl").setRequired(true).setLabel(Model.of("Bild-URL")).add(new UrlValidator()));
+    }
+
+}
