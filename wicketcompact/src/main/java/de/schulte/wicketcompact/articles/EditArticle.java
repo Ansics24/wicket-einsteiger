390611321424d10a43e9178a0d5aec63ffe62bca
Ansgar Schulte <info@ansgarschulte-itconsulting.de>
2018-11-18 13:49:25 +0100

    added external link in article form to wikipedia

--- wicketcompact/src/main/java/de/schulte/wicketcompact/articles/EditArticle.java
+++ wicketcompact/src/main/java/de/schulte/wicketcompact/articles/EditArticle.java
@@ -7,6 +7,7 @@ import de.schulte.wicketcompact.services.ArticleService;
 import de.schulte.wicketcompact.services.ServiceRegistry;
 import org.apache.wicket.bean.validation.PropertyValidator;
 import org.apache.wicket.markup.html.form.*;
+import org.apache.wicket.markup.html.link.ExternalLink;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.CompoundPropertyModel;
 import org.apache.wicket.model.Model;
@@ -26,21 +27,31 @@ public class EditArticle extends Panel {
         }
     };
 
+    private final TextField<String> nameField;
+
     public EditArticle(String id) {
         super(id);
         form.setModel(new CompoundPropertyModel<>(new EntityModel<>(ArticleService.class)));
+        this.nameField = new TextField<>("name");
     }
 
     @Override
     protected void onInitialize() {
         super.onInitialize();
         initializeForm();
+        add(new ExternalLink("help", new Model<String>() {
+
+            @Override
+            public String getObject() {
+                return "https://de.wikipedia.org/wiki/" + EditArticle.this.nameField.getModelObject();
+            }
+        }));
     }
 
     private void initializeForm() {
         add(form);
         add(new ValidationErrorFeedbackPanel("validationFeedback"));
-        form.add(new TextField<String>("name").add(new PropertyValidator<>()));
+        form.add(nameField.add(new PropertyValidator<>()));
         form.add(new DropDownChoice<>("category", new CategoryListModel(), new ChoiceRenderer<>("name", "id")).add(new PropertyValidator<>()));
         form.add(new TextArea<String>("description").add(new PropertyValidator<>()).setLabel(Model.of("Beschreibung")));
         form.add(new TextField<>("price").add(new PropertyValidator<>()).setLabel(Model.of("Preis")));
