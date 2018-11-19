851ae9e9edbbd187eeb37592857b86aeeaa508e6
Ansgar Schulte <info@ansgarschulte-itconsulting.de>
2018-11-19 20:50:47 +0100

    added AJAX checkbox to tables form to display QR-Code when orderable electronically

--- wicketcompact/src/main/java/de/schulte/wicketcompact/tables/EditTable.java
+++ wicketcompact/src/main/java/de/schulte/wicketcompact/tables/EditTable.java
@@ -5,13 +5,22 @@ import de.schulte.wicketcompact.ValidationErrorFeedbackPanel;
 import de.schulte.wicketcompact.entities.Table;
 import de.schulte.wicketcompact.services.ServiceRegistry;
 import de.schulte.wicketcompact.services.TableService;
+import org.apache.wicket.AttributeModifier;
+import org.apache.wicket.Component;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.form.AjaxCheckBox;
 import org.apache.wicket.bean.validation.PropertyValidator;
+import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
-import org.apache.wicket.markup.html.form.*;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.Radio;
+import org.apache.wicket.markup.html.form.RadioGroup;
+import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.CompoundPropertyModel;
+import org.apache.wicket.model.Model;
 
 import java.util.Arrays;
 
@@ -27,9 +36,21 @@ public class EditTable extends Panel {
         }
     };
 
+    private final Component qrCode = new WebMarkupContainer("qrCode").setOutputMarkupPlaceholderTag(true);
+
+    private AttributeModifier dataAttributeModifier;
+
     public EditTable(String id) {
         super(id);
         form.setModel(new CompoundPropertyModel<>(new EntityModel<>(TableService.class)));
+        this.dataAttributeModifier = new AttributeModifier("data", new Model<String>() {
+
+            @Override
+            public String getObject() {
+                final Long id = EditTable.this.form.getModelObject().getId();
+                return id != null ? String.format("https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=http://localhost:8000/table/%d", id) : null;
+            }
+        });
     }
 
     @Override
@@ -38,8 +59,19 @@ public class EditTable extends Panel {
         add(form);
         add(new ValidationErrorFeedbackPanel("validationFeedback"));
         form.add(new TextField<String>("name").add(new PropertyValidator<>()));
-        form.add(new CheckBox("orderableElectronically").add(new PropertyValidator<>()));
+        form.add(new AjaxCheckBox("orderableElectronically") {
+
+            @Override
+            protected void onUpdate(AjaxRequestTarget target) {
+                EditTable.this.qrCode.setVisible(this.getModelObject());
+                target.add(EditTable.this.qrCode);
+            }
+
+        }.add(new PropertyValidator<>()));
         addSeatCountChoiceToForm();
+        this.form.add(this.qrCode);
+        this.qrCode.setVisible(this.form.getModelObject().getId() != null);
+        this.qrCode.add(dataAttributeModifier);
     }
 
     private void addSeatCountChoiceToForm() {
