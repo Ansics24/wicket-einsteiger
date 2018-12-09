77d25b4b89832267033d7aacc977bf967e9e491f
Ansgar Schulte <info@ansgarschulte-itconsulting.de>
2018-12-09 14:13:44 +0100

    FormComponentUpdatingBehavior and ajaxified bahavior added to article edit panel

--- wicketcompact/src/main/java/de/schulte/wicketcompact/articles/EditArticle.java
+++ wicketcompact/src/main/java/de/schulte/wicketcompact/articles/EditArticle.java
@@ -5,7 +5,12 @@ import de.schulte.wicketcompact.ValidationErrorFeedbackPanel;
 import de.schulte.wicketcompact.entities.Article;
 import de.schulte.wicketcompact.services.ArticleService;
 import de.schulte.wicketcompact.services.ServiceRegistry;
+import org.apache.wicket.AttributeModifier;
+import org.apache.wicket.MarkupContainer;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.form.AjaxFormComponentUpdatingBehavior;
 import org.apache.wicket.bean.validation.PropertyValidator;
+import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.form.*;
 import org.apache.wicket.markup.html.link.ExternalLink;
 import org.apache.wicket.markup.html.panel.Panel;
@@ -29,23 +34,52 @@ public class EditArticle extends Panel {
 
     private final TextField<String> nameField;
 
+    private final MarkupContainer image;
+
+    private final FormComponent<String> imageUrl;
+
+    private final ExternalLink helpLink;
+
     public EditArticle(String id) {
         super(id);
         form.setModel(new CompoundPropertyModel<>(new EntityModel<>(ArticleService.class)));
+        this.helpLink = new ExternalLink("help", new Model<String>() {
+
+            @Override
+            public String getObject() {
+                return "https://de.wikipedia.org/wiki/" + EditArticle.this.nameField.getModelObject();
+            }
+        });
+        this.helpLink.setOutputMarkupPlaceholderTag(true);
         this.nameField = new TextField<>("name");
+        this.nameField.add(new AjaxFormComponentUpdatingBehavior("change") {
+            @Override
+            protected void onUpdate(AjaxRequestTarget target) {
+                target.add(helpLink);
+            }
+        });
+        this.image = new WebMarkupContainer("image");
+        this.image.add(new AttributeModifier("src", new Model<String>() {
+            @Override
+            public String getObject() {
+                return imageUrl.getModelObject();
+            }
+        }));
+        this.imageUrl = new TextField<String>("imageUrl").add(new PropertyValidator<>()).setLabel(Model.of("Bild-URL"));
+        this.imageUrl.add(new AjaxFormComponentUpdatingBehavior("change") {
+            @Override
+            protected void onUpdate(AjaxRequestTarget target) {
+                target.add(image);
+            }
+        });
+        this.image.setOutputMarkupPlaceholderTag(true);
     }
 
     @Override
     protected void onInitialize() {
         super.onInitialize();
         initializeForm();
-        this.form.add(new ExternalLink("help", new Model<String>() {
-
-            @Override
-            public String getObject() {
-                return "https://de.wikipedia.org/wiki/" + EditArticle.this.nameField.getModelObject();
-            }
-        }));
+        this.form.add(this.helpLink);
     }
 
     private void initializeForm() {
@@ -57,7 +91,8 @@ public class EditArticle extends Panel {
         form.add(new TextField<>("price").add(new PropertyValidator<>()).setLabel(Model.of("Preis")));
         form.add(new TextField<>("validFrom").setLabel(Model.of("Gültig ab")).add(RangeValidator.maximum(LocalDate.now().plusMonths(3))));
         form.add(new TextField<>("validTo").setLabel(Model.of("Gültig bis")).add(RangeValidator.minimum(LocalDate.now().plusDays(1))));
-        form.add(new TextField<String>("imageUrl").add(new PropertyValidator<>()).setLabel(Model.of("Bild-URL")));
+        form.add(this.imageUrl);
+        form.add(this.image);
     }
 
     EditArticle setArticle(Article article) {
